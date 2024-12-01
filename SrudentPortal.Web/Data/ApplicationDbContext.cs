e3a5771d5b6f875c47d3519a8679db4c3a628ddb
Victoria Baeva <baeva3112@gmail.com>
2024-12-01 17:40:58 +0300

    Добавьте файлы проекта.

---
+++ SrudentPortal.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using SrudentPortal.Web.Models.Entities;
+
+namespace SrudentPortal.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+
+        }
+
+        public DbSet<Student> Student { get; set; }
+    }
+}
